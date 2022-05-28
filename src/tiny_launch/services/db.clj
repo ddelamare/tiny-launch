46d39902973a35ff15159e4b81124d740b7862e6
ddelam <daniel.delamare@live.com>
2022-05-27 22:29:05 -0700

    Added site insert

--- src/tiny_launch/services/db.clj
+++ src/tiny_launch/services/db.clj
@@ -1,8 +1,27 @@
 (ns tiny-launch.services.db
   (:require [monger.core :as mg]
             [monger.collection :as mc])
-  (:import [com.mongodb MongoOptions ServerAddress]))
+  (:import [com.mongodb MongoOptions ServerAddress]
+           [org.bson.types ObjectId]))
 
 (def conn (atom (mg/connect)))
 
 (def main-db (atom (mg/get-db @conn "tiny-launch")))
+
+(defn get-sites
+  [filter]
+  (mc/find-maps @main-db "sites" filter))
+
+(defn ->site-model
+  [obj]
+  {
+   :_id (:_id obj)
+   :label (:label obj)
+   :created (:created obj)
+  })
+
+(defn upsert-site
+  [site]
+  (let [id (or (:_id site) (ObjectId.))]
+    (mc/update @main-db "sites" {:_id id} 
+               (assoc (assoc (->site-model site) :_id id) :created (or (:created site) (java.util.Date.))) {:upsert true})))
--- src/tiny_launch/services/site.clj
+++ src/tiny_launch/services/site.clj
@@ -1,11 +1,13 @@
 (ns tiny-launch.services.site
-  (:require [monger.core :as mg])
-  (:import [com.mongodb MongoOptions ServerAddress]))
+  (:require [monger.core :as mg]
+            [tiny-launch.services.db :as db])
+  (:import [com.mongodb MongoOptions ServerAddress]
+           [org.bson.types ObjectId]))
 
 (defn get-site 
   "Gets a list of sites based on the keyword"
   [keyword]
   (case keyword
-    :featured '({:label "hi"} {:label "bye"})
+    :featured (tiny-launch.services.db/get-sites {})
     :newest '({:label "Dodododot"} {:label "Baby Sharky"} {:label "My Tes site"})
     nil '({:label "You get nothing!"})))
