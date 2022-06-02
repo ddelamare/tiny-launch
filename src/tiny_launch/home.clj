24b1d0c133ad7bf0b571bc120d3c5c44080bf363
ddelam <daniel.delamare@live.com>
2022-06-02 16:29:32 -0700

    Added profile links

--- src/tiny_launch/home.clj
+++ src/tiny_launch/home.clj
@@ -1,5 +1,6 @@
 (ns tiny-launch.home
   (:require [hiccup.page :as p]
+            [hiccup.element :as e]
             [tiny-launch.layout :as l]
             [tiny-launch.services.site :as ss]))
 
@@ -7,7 +8,7 @@
   "Lists a set of sites. Takes parameters for type and page size and paging"
   [list-type pageSize pageable?]
   [:div.site-list
-   [:div.list-box.striped (map (fn [kv] [:div.site-list-row (:label kv)]) (ss/get-site list-type))]])
+   [:div.list-box.striped (map (fn [kv] (e/link-to (l/route-builder :site-profile :params {:site-id (:_id kv)}) [:div.site-list-row (:label kv)])) (ss/get-site list-type))]])
 
 (defn home-template [request]
   (l/layout request
--- src/tiny_launch/sites.clj
+++ src/tiny_launch/sites.clj
@@ -1,7 +1,8 @@
 (ns tiny-launch.sites
   (:require [hiccup.page :as p]
             [tiny-launch.layout :as l]
-            [tiny-launch.services.site :as ss]))
+            [tiny-launch.services.site :as ss]
+            [tiny-launch.services.db :as db]))
 
 (defn site-list
   "Lists a set of sites. Takes parameters for type and page size and paging"
@@ -15,7 +16,7 @@
     (p/html5
    [:head (prn request)
     (p/include-css "/css/site-profile.css")]
-     (if-let [site (tiny-launch.services.db/get-site (:site-id (:path-params request)))]
+     (when-let [site (db/get-site (:site-id (:path-params request)))]
        [:div.content
         [:div.hero [:h1 (:label site)]]
         [:div.featured "This is what my site does and why you need it"]
