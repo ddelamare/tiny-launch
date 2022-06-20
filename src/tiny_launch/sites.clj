930e3393f5b0e6fc233feea98dabbaafcba3dd2d
ddelam <daniel.delamare@live.com>
2022-06-20 14:23:43 -0700

    Added form macro

--- src/tiny_launch/sites.clj
+++ src/tiny_launch/sites.clj
@@ -14,10 +14,13 @@
 
 (defn form-field
   "Takes a label and a model and generates an input or static text"
-  [label site isEdit]
-  (if isEdit
-    (f/text-field label ((keyword label) site))
-    ((keyword label) site)))
+  [label site isEdit & {:keys [type] :or {type :text}}] ;; Allows for keyed/named parameters with defaults
+  (let [formMethod (case type
+                     :text f/text-field
+                     :textarea f/text-area)]
+    (if isEdit
+      (formMethod label ((keyword label) site))
+      ((keyword label) site))))
 
 (defn profile-template
   [request]
@@ -27,12 +30,13 @@
               (p/include-css "/css/site-profile.css")]
              (when-let [site (db/get-site (:site-id (:path-params request)))]
                (let [isEdit (= "1" (:edit (:params request)))]
-                 [:div.content
-                  (if isEdit
-                    [:div [:button "Save"] [:button (e/link-to (@(:url-for request) :site-profile :params {}) "Cancel")]]
-                    [:button (e/link-to (@(:url-for request) :site-profile :params {:edit 1}) "Edit")])
-                  [:div.hero [:h1 (form-field :label site isEdit)]]
-                  [:div.featured (:description site)]
-                  [:div.newest "See some pictures I took"]
-                  [:div.spiel "Doesn't this make you want to buy it?" (str request)]])))))
+                  (f/form-to [:put "/site/:site-id/update"]
+                             [:div.content
+                              (if isEdit
+                                [:div [:button "Save"] [:button (e/link-to (@(:url-for request) :site-profile :params {}) "Cancel")]]
+                                [:button (e/link-to (@(:url-for request) :site-profile :params {:edit 1}) "Edit")])
+                              [:div.hero [:h1 (form-field :label site isEdit)]]
+                              [:div.featured (form-field :description site isEdit :type :textarea)]
+                              [:div.newest "See some pictures I took"]
+                              [:div.spiel "Doesn't this make you want to buy it?" (str request)]]))))))
 
